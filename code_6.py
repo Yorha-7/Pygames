5dfd01acd1554396f48cf9e58d2ff3e9f6ec4e07
Yorha-7 <scientistn1420@gmail.com>
2025-08-27 13:36:23 +0530

    update

--- code_6.py
+++ code_6.py
@@ -10,9 +10,10 @@ kp,ki,kd = 0.01,0,0
 velocity = 1
 clk = 60
 cf = 0
-setpoint = 800
+setpoint = 700
 pid_val = 0
 curr_pos,prev_pos = 0,0
+
 def pid():
     global cf,clk,kp,ki,kd,error
     error = setpoint - object_rect.x
@@ -25,11 +26,15 @@ def move_obj():
     return object_rect
 
 #py objects and surfaces
+font = pygame.font.Font(None,30)
+text_frame = font.render('Finish',None,"Green")
 screen = pygame.display.set_mode((length,weidth))
 pygame.display.set_caption("Pid tester")
 object_surface = pygame.Surface((2,2))
 object_surface.fill('green')
 object_rect = object_surface.get_rect(topleft = (0,350))
+finish_surface = pygame.Surface((2,2))
+finish_surface.fill('red')
 clock = pygame.time.Clock()
 
 while True:
@@ -45,6 +50,8 @@ while True:
     if current_pos - prev_pos != 0:
         pid_val = 0
     screen.blit(object_surface,object_rect)
+    screen.blit(text_frame,(setpoint+2,weidth/2))         #gui for finish point
+    screen.blit(finish_surface,(setpoint,weidth/2))    # dot to identify finish point
     clock.tick(clk)
     pygame.display.update()
 
